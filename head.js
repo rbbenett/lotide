4c3389480f700a38832326c521c51ae1271fcc36
Robert <rbbenett0614@gmail.com>
2020-11-23 20:39:22 +0000

    added Head

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀️😀️😀️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭️😭️😭️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
