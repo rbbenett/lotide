da31056fe12567b34bededeae49cbddb4eed465e
Robert <rbbenett0614@gmail.com>
2020-11-24 21:45:17 +0000

    first commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀️😀️😀️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭️😭️😭️Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqualArrays = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
