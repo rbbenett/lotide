137cf619dd7a5759429f08d7dcab070d2cc1d699
Robert <rbbenett0614@gmail.com>
2020-11-25 17:25:55 +0000

    Second Commit

--- without.js
+++ without.js
@@ -1,22 +1,22 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
     return false;
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
       return false;
     }
   }
   return true;
 };
 
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
     return true;
   } else {
     return false;
   }
-};
+}
 
 const without = function(source, itemsToRemove) {
   let newArray = [];
