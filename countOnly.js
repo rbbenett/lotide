cee32dc9debebe6238ec45b2d2a097abd17f29eb
Robert <rbbenett0614@gmail.com>
2020-11-25 19:54:32 +0000

    Second Commit

--- countOnly.js
+++ countOnly.js
@@ -6,3 +6,26 @@ const assertEqual = function(actual, expected) {
   }
 
 };
+
+const countOnly = function(allItems, itemsToCount) {
+
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
