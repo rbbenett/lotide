3a67ce2d39ff4732124aeb49cfe3333ea0146652
Robert <rbbenett0614@gmail.com>
2020-11-25 21:25:51 +0000

    first commit

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀️😀️😀️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭️😭️😭️Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(inputString) {
+  
+  const results = {};
+  for (let letter of inputString) {
+    if (letter === ' ') continue; //skip spaces
+
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse"))
