e45f7857e19b20b0a074130a00e9b53b52c14f78
Robert <rbbenett0614@gmail.com>
2020-11-25 19:51:31 +0000

    First Commit

---
+++ countOnly.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀️😀️😀️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭️😭️😭️Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
