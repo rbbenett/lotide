06cf041b7d5a1e0d6cdebee903b460f531085852
Robert <rbbenett0614@gmail.com>
2020-11-26 20:01:03 +0000

    First Commit

---
+++ takeUntil.js
@@ -0,0 +1,19 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else break;
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
