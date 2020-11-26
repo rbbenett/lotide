53a93b4ac83fc75db328217aa4d201cb4d00a58a
Robert <rbbenett0614@gmail.com>
2020-11-26 19:07:55 +0000

    final commit

--- map.js
+++ map.js
@@ -16,17 +16,17 @@ const assertArraysEqual = function(actual, expected) {
   } else {
     return false;
   }
-}
+};
 
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
   const results = [];
-  for(let item of array) {
+  for (let item of array) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
 const results1 = map(words, word => word[0]);
 // console.log(results1);
