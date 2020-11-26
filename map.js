ad1fc75257af0a52b0307e53c0caa70ff33ae484
Robert <rbbenett0614@gmail.com>
2020-11-26 18:35:05 +0000

    first commit

---
+++ map.js
@@ -0,0 +1,10 @@
+const map = function(array, callback) {
+  const words = ["ground", "control", "to", "major", "tom"];
+}
+
+const map = function() {
+  // empty for now :)
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
