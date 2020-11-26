c3ff441dc300d63dd71b3a04b5a8030930b0732b
Robert <rbbenett0614@gmail.com>
2020-11-26 18:55:52 +0000

    fourth commit

--- map.js
+++ map.js
@@ -1,14 +1,36 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
   const results = [];
   for(let item of array) {
-    console.log('item BEFORE: ', item);
-    console.log('item AFTER: ', callback(item));
-    console.log('---');
+    results.push(callback(item));
   }
   return results;
 }
 
 const results1 = map(words, word => word[0]);
-console.log(results1);
+// console.log(results1);
+
+console.log(assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]));
+console.log(assertArraysEqual(results1, [1, 2, 3, 4, 5]));
+console.log(assertArraysEqual(results1, ['g', 'c', 't', 'm', 'T']));
