0c339dd741959371bc56e6ea056c0cfe4260bcf1
Robert <rbbenett0614@gmail.com>
2020-11-26 18:43:44 +0000

    third commit

--- map.js
+++ map.js
@@ -1,13 +1,13 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
-  // temporary code:
-  console.log('array: ', array);
-  console.log('callback: ', callback);
-  
-  
   const results = [];
-  return results
+  for(let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+  return results;
 }
 
 const results1 = map(words, word => word[0]);
