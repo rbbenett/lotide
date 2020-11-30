f5fdee914ecacddc983fa25c11f03af1694a1fb2
Robert <rbbenett0614@gmail.com>
2020-11-30 18:09:56 +0000

    Refractored Commit

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀️😀️😀️Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😭️😭️😭️Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
@@ -18,8 +9,6 @@ const eqArrays = function(actual, expected) {
   }
   return true;
 };
-    
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
 
---
+++ test/eqArraysTest.js
@@ -0,0 +1,5 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
